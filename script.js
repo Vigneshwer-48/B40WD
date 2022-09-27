1d281223c17bee529bf83c8d2bfe6f76b1747ccb
Vigneshwer-48 <vickynanban48@gmail.com>
2022-09-27 11:48:03 +0530

    New Commit

--- script.js
+++ script.js
@@ -14,9 +14,18 @@ request.send();
 request.onload=function(){
     var result=JSON.parse(request.response);
     console.log(result);
-    for(var i=0;i<result.length;i++){
-        console.log(result[i].name);
-    }
+    // for(var i=0;i<result.length;i++){
+    //     console.log(result[i].name);
+    // }
+    var pop=result.filter((ele)=>ele.population<300000);
+    console.log(pop);
+    var result1=pop.map((ele)=>`${ele.name} : ${ele.region}`);
+    console.log(result1);
+
+    var region=result.filter((ele)=>ele.region=="Europe");
+    console.log(region);
+    var result2=region.map((ele)=>ele.name);
+    console.log(result2);
 };
 
 
