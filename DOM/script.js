47006457f4c8c77bedf989c15daa87278bf8d68e
Vigneshwer-48 <vickynanban48@gmail.com>
2022-10-13 12:29:05 +0530

    New commit

--- DOM/script.js
+++ DOM/script.js
@@ -28,6 +28,30 @@
 // element.append(element1);
 // document.body.append(ele);
 
+// function foo(){
+//     var res=document.getElementById("first name").value;
+//     var res1=document.getElementById("middle name").value;
+//     var res2=document.getElementById("last name").value;
+//     var res3=document.getElementById("email").value;
+//     var res4=document.getElementById("password").value;
+
+//      console.log(res,res1,res2,res3,res4);
+// }
+
+// let button=document.createElement("button");
+// button.innerHTML="Click Me";
+// button.addEventListener("click",foo);
+// document.body.append(button);
+
+// function foo(){
+//     let ele=document.createElement("span");
+//     ele.innerHTML="Welcom to Guvi";
+//     ele.style.color="blue";
+//     ele.style.fontSize="20px";
+//     document.body.append(ele);
+// }
+
+
 // var fname=createlabels("label","for","firstname","First Name");
 // var br1=createlinebreak("br");
 // var input=createinput("input","type","firstname","id","First Name");
@@ -67,12 +91,35 @@
 //     return ele;
 // }
 
-function foo(){
-    var res=document.getElementById("first name").value;
-    var res1=document.getElementById("middle name").value;
-    var res2=document.getElementById("last name").value;
-    var res3=document.getElementById("email").value;
-    var res4=document.getElementById("password").value;
+var table=document.createElement("table");
+table.setAttribute("class","table");
+var thead=document.createElement("thead");
+thead.setAttribute("class","thead-dark");
+var tr=document.createElement("tr");
+var th1=document.createElement("th");
+th1.setAttribute("scope","col");
+th1.innerHTML="First";
+var th2=document.createElement("th");
+th2.setAttribute("scope","col");
+th2.innerHTML="Middle";
+var th3=document.createElement("th");
+th3.setAttribute("scope","col");
+th3.innerHTML="Last";
+ var tbody=document.createElement("tbody");
+ tbody.setAttribute("class","tbody");
+ var tr=document.createElement("tr");
+ var td1=document.createElement("td");
+ td1.innerHTML="Mark";
+ var td2=document.createElement("td");
+ td2.innerHTML="Otto";
+ var td3=document.createElement("td");
+ td3.innerHTML="@mdo";
 
-     console.log(res,res1,res2,res3,res4);
-}
+ tr.append(td1,td2,td3);
+ tbody.append(tr);
+ table.append(tbody);
+ document.body.append(table);
+
+
+
+
